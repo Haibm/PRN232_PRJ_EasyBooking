b3d329e325d50ddd6d3ce0213ea3c0ff14293f4e
trungPhamDuc <ngokinhvodich@gmail.com>
2025-07-25 10:40:42 +0700

    admin bussiness dashboard

--- PRN232_PRJ_EasyBooking/EasyBooking.Web/obj/Debug/net8.0/EasyBooking.Web.AssemblyInfo.cs
+++ PRN232_PRJ_EasyBooking/EasyBooking.Web/obj/Debug/net8.0/EasyBooking.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyBooking.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b197d667d0474117ca3bfbada4172d6d484d4868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bce8bf5fd502584840b6e16af81f9fca32db391")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyBooking.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyBooking.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
